66f6eab6717b88817077c1018e4a727cdbf2a366
Darren Spruell <phatbuckett@gmail.com>
2019-05-18 22:57:53 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='aslookup',
-    version='1.0.5',
+    version='1.0.6',
     description='IP to AS routing data query script',
     long_description=long_description,
     long_description_content_type='text/markdown',
